72387f3aacc21403db691b3895aecda9284a0c97
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-12-09 00:11:32 +0200

    Add files via upload

---
+++ 1.0 First Steps In Coding - Lab/02. Nums 1to10.js
@@ -0,0 +1,14 @@
+function num1To10() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i)
+    }
+}
+
+
+
+function Nums1To10() {
+    let i = 1
+    while (i !== 11) {
+      console.log(i++);  
+    }
+  }
---
+++ 1.0 First Steps In Coding - Lab/03. Square Area.js
@@ -0,0 +1,4 @@
+function squareArea(n) {
+    return n ** 2
+}
+
