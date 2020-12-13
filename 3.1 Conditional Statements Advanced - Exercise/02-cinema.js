0cd6b3d505787200bcd5b856f481882cdaaf673e
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-12-14 01:46:09 +0200

    Add files via upload

---
+++ 3.1 Conditional Statements Advanced - Exercise/02-cinema.js
@@ -0,0 +1,7 @@
+function cinema(...arg) {
+  let [type, row, col] = [...arg]
+  let projectionType = { Premiere: 12.0, Normal: 7.5, Discount: 5.0 };
+  return (projectionType[type] * row * col).toFixed(2) + " leva"
+}
+
+// console.log(cinema("Premiere", 10, 12))
